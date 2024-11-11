438ab034ab7e2f5f375e24f0d946228c55c63f93
Sreenivasulu3639 <sreenivasulupinnepalli@gmail.com>
2024-11-11 22:20:19 +0530

    second commit

--- Online_Food_Management/wsgi.py
+++ Online_Food_Management/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OnlineTiffinManagement project.
+WSGI config for Online_Food_Management project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineTiffinManagement.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Online_Food_Management.settings')
 
 application = get_wsgi_application()
